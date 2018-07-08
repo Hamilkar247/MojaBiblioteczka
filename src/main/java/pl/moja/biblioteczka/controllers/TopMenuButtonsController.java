e266e94327ae826e9a352cc08c9f122a5fe7bbb4
Hamilkar247 <adonnibal96@gmail.com>
2018-07-08 14:21:18 +0200

    dodanie dzialajacegozalczania pliku fxml po nacisnieuciu buttona

--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -6,23 +6,29 @@ import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    public static final String LIBRARY_FXML = "/fxml/Library.fxml";
+    public static final String LIST_BOOKS_FXML = "/fxml/ListBooks.fxml";
+    public static final String STATISTICS_FXML = "/fxml/Statistics.fxml";
+    public static final String ADD_BOOK_FXML = "/fxml/AddBook.fxml";
     @FXML
     private ToggleGroup toggleButtons;
 
 
     private MainController mainController;
 
+
+
     @FXML
     public void openLibrary() {
-        System.out.println("openLibrary");
+        mainController.setCenter(LIBRARY_FXML);
     }
     @FXML
     public void openListBook() {
-        System.out.println("openListBook");
+        mainController.setCenter(LIST_BOOKS_FXML);
     }
     @FXML
     public void openStatistics() {
-        System.out.println("openStatistic");
+        mainController.setCenter(STATISTICS_FXML);
     }
 
 
@@ -36,6 +42,6 @@ public class TopMenuButtonsController {
         if(toggleButtons.getSelectedToggle()!=null){
             toggleButtons.getSelectedToggle().setSelected(false);
         }
-        mainController.setCenter("/fxml/AddBook.fxml");
+        mainController.setCenter(ADD_BOOK_FXML); //metoda wstawiajaca tego fxmla w srodokowa czesc
     }
 }
