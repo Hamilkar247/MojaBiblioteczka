f72b6d30b3831bed8749a95854eb576947d19ad4
Hamilkar247 <adonnibal96@gmail.com>
2018-07-09 08:09:34 +0200

    menu u gory

--- src/main/java/pl/moja/biblioteczka/controllers/MainController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -1,9 +1,14 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
@@ -38,4 +43,27 @@ public class MainController {
         borderPane.setCenter(parent);
     }
 
+    public void closeApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void setCaspian() {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_CASPIAN);
+    }
+
+    public void setModena() {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+    }
+
+    public void setAlwaysOnTop(ActionEvent actionEvent) {
+        //dobieramy sie do stage
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+        boolean value =((CheckMenuItem) actionEvent.getSource()).selectedProperty().get(); //sprawdzam czy zawsze na wierzchu jest wcisniete czy nie
+        stage.setAlwaysOnTop(value);
+
+    }
+
+    public void about() {
+    }
 }
